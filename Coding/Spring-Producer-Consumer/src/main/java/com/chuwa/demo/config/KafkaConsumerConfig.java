e0d508a589a7e09d2a5372e3d454701af4836203
blackrabbitllc <sssongxinyue@gmail.com>
2024-07-31 08:49:53 -0700

    hw50 kafka q3

---
+++ Coding/Spring-Producer-Consumer/src/main/java/com/chuwa/demo/config/KafkaConsumerConfig.java
@@ -0,0 +1,38 @@
+package com.chuwa.demo.config;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaConsumerConfig {
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "myGroup");
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        return new DefaultKafkaConsumerFactory<>(props);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        factory.setConcurrency(3); // Setting up 3 consumers
+//        factory.setConcurrency(5);
+        return factory;
+    }
+}
---
+++ Coding/Spring-Producer-Consumer/src/main/java/com/chuwa/demo/controller/KafkaController.java
@@ -0,0 +1,25 @@
+package com.chuwa.demo.controller;
+
+import com.chuwa.demo.service.KafkaProducerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class KafkaController {
+
+    @Autowired
+    private KafkaProducerService kafkaProducerService;
+
+//    @PostMapping("/publish")
+//    public String publishMessage(@RequestParam("key") String key, @RequestParam("message") String message) {
+//        kafkaProducerService.sendMessage(key, message);
+//        return "Message published successfully";
+//    }
+    @PostMapping("/publish")
+    public String publishMessage(@RequestParam("message") String message) {
+        kafkaProducerService.sendMessage(message);
+        return "Message published successfully";
+    }
+}
